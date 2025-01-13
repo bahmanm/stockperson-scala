18af4aea6fc4f53b5411fe0869eca99f342fd852
Bahman Movaqar <bahman@bahmanm.com>
2025-01-12 20:07:27 -0800

    Configure scalafmt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,4 @@
 @main def hello(): Unit =
-  println("Hello world!")
   println(msg)
 
-def msg = "I was compiled by Scala 3. :)"
+def msg = "Hello, world"
---
+++ src/test/scala/MainSuite.scala
@@ -0,0 +1,7 @@
+// For more information on writing tests, see
+// https://scalameta.org/munit/docs/getting-started.html
+class MainSuite extends munit.FunSuite {
+  test("main") {
+    assertEquals(msg, "Hello, world")
+  }
+}
